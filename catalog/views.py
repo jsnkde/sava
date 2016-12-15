695dbe94c386f27722ebb933c78965211fa4657e
jsnk <jsnk@yandex.ru>
2016-12-15 22:03:24 +0300

    Search by tags and description.

--- catalog/views.py
+++ catalog/views.py
@@ -1,35 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic, View
+import re
+
 from django.urls import reverse
-from allauth.account.adapter import DefaultAccountAdapter
-from catalog.models import Location, User, Item
-from taggit.models import Tag
+from django.db.models import Q
 from django.db.models.aggregates import Count
+from allauth.account.adapter import DefaultAccountAdapter
+from taggit.models import Tag
+
+from catalog.models import Location, User, Item
 
 
 class IndexView(generic.ListView):
 	template_name = 'catalog/index.html'
 	context_object_name = 'items'
 
-	def get_queryset(self):
-		print self.request.GET.getlist('tag')
+	def get_location(self, request):
 		# Extract preserved location for authenticated users
-		if not self.request.user.is_anonymous and self.request.user.location is not None:
-			self.location = self.request.user.location
+		if not request.user.is_anonymous and request.user.location is not None:
+			self.location = request.user.location
 
 		# Check session
-		elif self.request.session.has_key('location'):
-			self.location = Location.objects.get(id=int(self.request.session['location']))
+		elif request.session.has_key('location'):
+			self.location = Location.objects.get(id=int(request.session['location']))
 
 		# Default location
 		else:
 			self.location = Location.objects.get(id=1) # TODO
 
+	def get_queryset(self):
+		self.get_location(self.request)
+		
 		items = Item.objects.filter(user__location=self.location)
 
-		if self.request.GET.has_key('tag'):
-			items = items.filter(tags__name__in=self.request.GET.getlist('tag'))
+		if self.request.GET.has_key('search') and len(self.request.GET['search']) > 0:
+			search = map(unicode.lower, re.findall(r'(\w+)', self.request.GET['search'], re.UNICODE))
+			items = items.filter(Q(tags__name__in=search) | Q(reduce(lambda x, y: x | y, [Q(description__icontains=word) for word in search]))).distinct()
 
 		return items
 
@@ -38,9 +45,6 @@ class IndexView(generic.ListView):
 		context['locations'] = Location.objects.all()
 		context['location'] = self.location
 		context['tags'] = Tag.objects.filter(item__user__location=self.location).annotate(count=Count('item')).order_by('-count')
-
-		# If DB sorting is not working properly try
-		# sorted(Location.objects.all(), key=operator.attrgetter('region','name'))
 
 		return context
 
